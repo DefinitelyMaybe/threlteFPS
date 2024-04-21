84a74d259520e3b61cd1ea49e4bfe51188d89210
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-04-22 11:47:34 +1200

    add modelpipeline, add map model, remove grid and floating geometries.

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { T, useThrelte } from '@threlte/core';
-	import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras';
-	import { AutoColliders } from '@threlte/rapier';
+	import { ContactShadows, OrbitControls } from '@threlte/extras';
 	import * as THREE from 'three';
+	import Map from './models/collision-world.svelte';
 
 	const { scene } = useThrelte();
 
@@ -18,36 +18,6 @@
 <T.AmbientLight intensity={0.2} />
 <T.HemisphereLight args={[0x8dc1de, 0x00668d, 1.5]} position={[2, 1, 1]} />
 
-<Grid
-	position.y={-0.001}
-	cellColor="#ffffff"
-	sectionColor="#ffffff"
-	sectionThickness={0}
-	cellSize={2}
-	infiniteGrid
-/>
+<Map />
 
 <ContactShadows scale={10} blur={2} far={2.5} opacity={0.5} />
-
-<Float floatIntensity={1} floatingRange={[0, 1]}>
-	<AutoColliders>
-		<T.Mesh position.y={1.2} position.z={-0.75}>
-			<T.BoxGeometry />
-			<T.MeshStandardMaterial color="#0059BA" />
-		</T.Mesh>
-	</AutoColliders>
-</Float>
-
-<Float floatIntensity={1} floatingRange={[0, 1]}>
-	<T.Mesh position={[1.2, 1.5, 0.75]} rotation.x={5} rotation.y={71}>
-		<T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-		<T.MeshStandardMaterial color="#F85122" />
-	</T.Mesh>
-</Float>
-
-<Float floatIntensity={1} floatingRange={[0, 1]}>
-	<T.Mesh position={[-1.4, 1.5, 0.75]} rotation={[-5, 128, 10]}>
-		<T.IcosahedronGeometry />
-		<T.MeshStandardMaterial color="#F8EBCE" />
-	</T.Mesh>
-</Float>
